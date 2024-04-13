c2018c8d3e2ef72f8a62a66d8970dc65fa2b1fa7
João Vitor Gomes Nunes <joaovitor.jvgn2002@gmail.com>
2024-04-13 20:52:46 -0300

    Ajustado aba de stacks

--- src/pages/home/Aboutme/index.js
+++ src/pages/home/Aboutme/index.js
@@ -11,26 +11,10 @@ const Aboutme = () => {
           <h2>Developer</h2>
           <br />
           <h3>
-            I'm João Vitor, a programming enthusiast dedicated to continuous
-            learning and overcoming challenges. I'm currently immersed in the
-            dynamic world of programming, exploring new technologies and growth
-            opportunities that allow me evolve every day as a developer.
+          My name is João Vitor, and I'm a programming enthusiast dedicated to continuous learning and overcoming challenges.<br />
+          Currently, I'm immersed in the dynamic world of programming, exploring new technologies and growth opportunities that allow me to evolve every day as a developer.
+          Always seeking to expand my knowledge and skills, I'm constantly on the lookout for new challenges and opportunities to improve and make a difference in the world of technology.
           </h3>
-          <br></br>
-          <h2>My Tech Stack</h2>
-          <br></br>
-          <div className="stacks">
-            <img src="https://camo.githubusercontent.com/d63d473e728e20a286d22bb2226a7bf45a2b9ac6c72c59c0e61e9730bfe4168c/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f48544d4c352d4533344632363f7374796c653d666f722d7468652d6261646765266c6f676f3d68746d6c35266c6f676f436f6c6f723d7768697465" alt="HTML5"/>
-            <img src="https://camo.githubusercontent.com/3a0f693cfa032ea4404e8e02d485599bd0d192282b921026e89d271aaa3d7565/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f435353332d3135373242363f7374796c653d666f722d7468652d6261646765266c6f676f3d63737333266c6f676f436f6c6f723d7768697465" alt="CSS"/>
-            <img src="https://camo.githubusercontent.com/93c855ae825c1757f3426f05a05f4949d3b786c5b22d0edb53143a9e8f8499f6/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4a6176615363726970742d3332333333303f7374796c653d666f722d7468652d6261646765266c6f676f3d6a617661736372697074266c6f676f436f6c6f723d463744463145" alt="JAVASCRIPT"/>
-            <img src="https://camo.githubusercontent.com/924446ea65193e79f9c710d414c2bdeaeb5f7a41714b1d4c3e1b83fc6db4907d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4a6176612d4544384230303f7374796c653d666f722d7468652d6261646765266c6f676f3d6f70656e6a646b266c6f676f436f6c6f723d7768697465" alt="JAVA"/>
-            <img src="https://camo.githubusercontent.com/4bde567a4772f994f22418e4505a1ac8dc6e6219100251aa79b7279e02c8bb07/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f537072696e672d3644423333463f7374796c653d666f722d7468652d6261646765266c6f676f3d737072696e67266c6f676f436f6c6f723d7768697465" alt="SPRING"/>
-            <img src="https://camo.githubusercontent.com/879423585ed087f3c973857c43ba7e7d84f52c993d2c937055726339fbf921d9/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f506f73746d616e2d4646364333373f7374796c653d666f722d7468652d6261646765266c6f676f3d506f73746d616e266c6f676f436f6c6f723d7768697465" alt="POSTMAN"/>
-            <img src="https://camo.githubusercontent.com/31f0d5ddaa8c6ed369dcb04074f3d69c076170a3483237539be406c50ac17e2f/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3230323332413f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d36314441464265" alt="REACT"/>
-            <img src="https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white" alt="MYSQL"/>
-            <img src="https://camo.githubusercontent.com/b13ed67c809178963ce9d538175b02649800772be1ce0cb02da5879e5614e236/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f426f6f7473747261702d3536334437433f7374796c653d666f722d7468652d6261646765266c6f676f3d626f6f747374726170266c6f676f436f6c6f723d7768697465" alt="BOOTSTRAP"/>
-            <img src="https://camo.githubusercontent.com/41326de293d3848e2ab0f29bf1680427128757fe6b586ceddf1097cb4eeb5ff7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f7374796c65642d2d636f6d706f6e656e74732d4442373039333f7374796c653d666f722d7468652d6261646765266c6f676f3d7374796c65642d636f6d706f6e656e7473266c6f676f436f6c6f723d7768697465" alt="STYLED-COMPONENTS"/>
-          </div>
         </div>
       </div>
     </section>
